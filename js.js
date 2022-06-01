8876e66f959fad87cdbd00c0acb3ebf521e4c754
Bushra AlQahtani <89680878+bushra-alqahtani@users.noreply.github.com>
2022-06-02 01:04:56 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,29 @@
+var count1= 9;
+var countElement1 = document.querySelector("#count1");
+console.log(countElement1);
+
+function add1() {
+    count1++;
+    countElement1.innerHTML = count1 +"likes";
+    console.log(count1);
+}
+
+var count2= 12;
+var countElement2 = document.querySelector("#count2");
+console.log(countElement2);
+
+function add2() {
+    count2++;
+    countElement2.innerHTML = count2 +"likes";
+    console.log(count2);
+}
+
+var count3= 9;
+var countElement3 = document.querySelector("#count3");
+console.log(countElement3);
+
+function add3() {
+    count3++;
+    countElement3.innerHTML = count3 +"likes";
+    console.log(count3);
+}
